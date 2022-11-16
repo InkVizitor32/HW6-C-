323664faeac52f10321b0f9d9a7a9dcd5eef4456
InkVizitor32 <morgunov.roman89@gmail.com>
2022-11-16 22:17:01 +0300

    docs: init HW6

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//            Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//             0, 7, 8, -2, -2 -> 2
+//             1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateFillArray()
+{
+
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"введите {(i + 1)}-е число ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        arr[i] = a;
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+void FindCount(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    } 
+    Console.WriteLine($"количество положительных чисел равно {count}");
+}
+int[] mass = CreateFillArray();
+Console.WriteLine();
+PrintArray(mass);
+Console.WriteLine();
+FindCount(mass);
+Console.WriteLine();
